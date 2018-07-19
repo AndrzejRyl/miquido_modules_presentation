dc10accfa5d5d4d52c6521b11c3adebdf77cf014
Andrzej Ryl <andrzej.ryl@miquido.com>
2018-07-19 16:02:25 +0200

    Add tests for caching

--- ModulesExample/app/src/test/kotlin/com/fleenmobile/modulesexample/main/presenter/MainActivityPresenterTest.kt
+++ ModulesExample/app/src/test/kotlin/com/fleenmobile/modulesexample/main/presenter/MainActivityPresenterTest.kt
@@ -1,5 +1,6 @@
 package com.fleenmobile.modulesexample.main.presenter
 
+import com.fleenmobile.core.configuration.Configuration
 import com.fleenmobile.modulesexample.main.MainActivityContract
 import com.fleenmobile.testcore.BaseTest
 import org.junit.Test
@@ -14,23 +15,26 @@ class MainActivityPresenterTest : BaseTest() {
   @Mock
   lateinit var router: MainActivityContract.Router
 
+  @Mock
+  lateinit var configuration: Configuration
+
   private val mockCounter = 0
   private lateinit var presenter: MainActivityContract.Presenter
 
   override fun setup() {
     super.setup()
 
-    presenter = MainActivityPresenter(view, router)
+    presenter = MainActivityPresenter(view, router, configuration)
   }
 
   override fun tearDown() {
     super.tearDown()
-    verifyNoMoreInteractions(view, router)
+    verifyNoMoreInteractions(view, router, configuration)
   }
 
   @Test
   fun `should increase counter on plus button click`() {
-    mockPositiveCounterResponse() // TODO mock caching service when it's implemented
+    mockPositiveCounterResponse()
 
     presenter.onPlusClicked()
 
@@ -39,7 +43,7 @@ class MainActivityPresenterTest : BaseTest() {
 
   @Test
   fun `should decrease counter on minus button click`() {
-    mockPositiveCounterResponse() // TODO mock caching service when it's implemented
+    mockPositiveCounterResponse()
 
     presenter.onMinusClicked()
 
@@ -48,29 +52,30 @@ class MainActivityPresenterTest : BaseTest() {
 
   @Test
   fun `should initialize view`() {
-    mockPositiveCounterResponse() // TODO mock caching service when it's implemented
+    mockPositiveCounterResponse()
+    val counter = 4
 
     presenter.visible(counter)
 
 
-    verify(view, times(1)).showCounter(mockCounter)
+    verify(view, times(1)).showCounter(counter)
   }
 
   private fun mockPositiveCounterResponse() {
-    // TODO mock caching service when it's implemented
+    `when`(configuration.counter).thenReturn(mockCounter)
   }
 
   @Test
   fun `should cache counter on clear`() {
     presenter.clear()
 
-    // todo add verification for caching service
+    verify(configuration, times(1)).counter = 0
   }
 
   @Test
   fun `should navigate to settings on settings button click`() {
     presenter.onSettingsClicked()
 
-    verify(router, times(1)).navigateToSettings()
+    verify(router, times(1)).navigateToSettings(0)
   }
 }
