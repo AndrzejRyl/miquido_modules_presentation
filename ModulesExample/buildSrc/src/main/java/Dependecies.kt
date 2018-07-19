1f5b974b001adb931773935b65658f05db5ea633
Andrzej Ryl <andrzej.ryl@miquido.com>
2018-07-19 16:34:20 +0200

    Remove unused resources

--- ModulesExample/buildSrc/src/main/java/Dependecies.kt
+++ ModulesExample/buildSrc/src/main/java/Dependecies.kt
@@ -6,8 +6,6 @@ object Versions {
   const val kotlin = "1.2.30"
   const val support = "27.1.1"
   const val junit = "4.12"
-  const val testRunner = "1.0.2"
-  const val espresso = "3.0.2"
   const val mockito = "2.16.0"
   const val fabric = "1.+"
   const val crashlytics = "2.9.1"
@@ -16,11 +14,8 @@ object Versions {
   const val dagger2 = "2.14.1"
   const val rxAndroid = "2.0.2"
   const val rxJava = "2.1.11"
-  const val retrofit = "2.4.0"
   const val stetho = "1.5.0"
   const val constraintLayout = "1.1.2"
-  const val glide = "4.7.1"
-  const val joda = "2.9.9"
   const val anko = "0.10.2"
 }
 
@@ -44,23 +39,14 @@ object Libs {
   const val dagger2AndroidCompiler = "com.google.dagger:dagger-android-processor:${Versions.dagger2}"
   const val rxAndroid = "io.reactivex.rxjava2:rxandroid:${Versions.rxAndroid}"
   const val rxJava = "io.reactivex.rxjava2:rxjava:${Versions.rxJava}"
-  const val retrofit = "com.squareup.retrofit2:retrofit:${Versions.retrofit}"
-  const val retrofitGsonConverter = "com.squareup.retrofit2:converter-gson:${Versions.retrofit}"
-  const val retrofitRxAdapter = "com.squareup.retrofit2:adapter-rxjava2:${Versions.retrofit}"
   const val stetho = "com.facebook.stetho:stetho:${Versions.stetho}"
   const val stethoNetwork = "com.facebook.stetho:stetho-okhttp3:${Versions.stetho}"
   const val constraintLayout = "com.android.support.constraint:constraint-layout:${Versions.constraintLayout}"
-  const val glide = "com.github.bumptech.glide:glide:${Versions.glide}"
-  const val glideProcessor = "om.github.bumptech.glide:compiler:${Versions.glide}"
-  const val joda = "net.danlew:android.joda:${Versions.joda}"
   const val anko = "org.jetbrains.anko:anko:${Versions.anko}"
   const val anko_commons = "org.jetbrains.anko:anko-commons:${Versions.anko}"
 }
 
 object TestLibs {
   const val junit = "junit:junit:${Versions.junit}"
-  const val testRunner = "com.android.support.test:runner:${Versions.testRunner}"
-  const val espressoCore = "com.android.support.test.espresso:espresso-core:${Versions.espresso}"
   const val mockito = "org.mockito:mockito-core:${Versions.mockito}"
-  const val joda = "joda-time:joda-time:${Versions.joda}"
 }
