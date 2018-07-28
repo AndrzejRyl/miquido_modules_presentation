7cf4d3f00f9085df16e545ac4eb95dd3b77f68bb
Andrzej Ryl <andrzej.ryl@gmail.com>
2018-07-28 13:19:57 +0200

    Fix package names in tests

--- {{ cookiecutter.repo_name }}/app/src/androidTest/kotlin/{{ cookiecutter.app_package_dir }}/ExampleInstrumentedTest.kt
+++ {{ cookiecutter.repo_name }}/app/src/androidTest/kotlin/{{ cookiecutter.app_package_dir }}/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.fleenmobile.modulesexample
+package {{ cookiecutter.app_package_name }}
 
 import android.support.test.InstrumentationRegistry
 import android.support.test.runner.AndroidJUnit4
